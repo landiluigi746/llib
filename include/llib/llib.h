a6f36b7aacad9edf84a9377c2f78874413406aa5
landiluigi746 <landiluigi865@gmail.com>
2024-07-09 20:22:18 +0200

    llib.h extern c moved

--- include/llib/llib.h
+++ include/llib/llib.h
@@ -9,16 +9,16 @@
 #ifndef LLIB_H__
 #define LLIB_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <llib/llib_memory.h>
 #include <llib/llib_algorithms.h>
 #include <llib/llib_memory.h>
 #include <llib/llib_io.h>
 #include <llib/llib_string.h>
 #include <llib/llib_utils.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 #ifdef __cplusplus
 }
